fa48f15aafa62edc105c47b0a7c8c7aaa3758451
noticetr <yusuf_ozgenc2001@hotmail.com>
2016-07-12 11:46:50 +0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -51,9 +51,9 @@
       chatLink: "https://rawgit.com/basicBot/source/master/lang/en.json",
       scriptLink: "https://rawgit.com/basicBot/source/master/basicBot.js",
       roomLock: false, // Requires an extension to re-load the script
-      startupCap: 1, // 1-200
-      startupVolume: 0, // 0-100
-      startupEmoji: false, // true or false
+      startupCap: 50, // 1-200
+      startupVolume: 5*, // 0-100
+      startupEmoji: true, // true or false
       autowoot: true,
       autoskip: false,
       smartSkip: true,
@@ -73,7 +73,7 @@
       historySkip: false,
       timeGuard: true,
       maximumSongLength: 10,
-      autodisable: true,
+      autodisable: false,
       commandCooldown: 30,
       usercommandsEnabled: true,
       skipPosition: 3,
